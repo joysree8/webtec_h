ebd2971f6eceef26285ac7bd581615c55912967f
Joysree Dey Sukanna <113924105+joysree8@users.noreply.github.com>
2023-03-15 22:02:44 +0600

    Add files via upload

---
+++ Lab_1/Lab_Task-1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>First Lab Task</title>
+</head>
+<style>
+	.div1 {
+		margin: 75px;
+	}
+	.div2{
+		margin:100px;
+	}
+</style>
+<body>
+
+<div class="div2">
+<form action="" method="post">
+    <p> Your Name: </p>
+    <input type="text" name="name1" placeholder="First Name"  id="">
+    <input type="text" name="name2" placeholder="Last Name" id="">
+   
+    <p>Birth Date :</p>
+		<input type="date">
+		<p>Gender :</p>
+		<input type="checkbox" name="male">Male
+		<input type="checkbox" name="female">Female
+		<p>Address :</p>
+		<input type="text" name="house" placeholder="House" id="">
+		<input type="text" name="street" placeholder="Street" id=""><br>
+		<input type="text" name="city" placeholder="City" id="">
+		<input type="text" name="state" placeholder="State / Province" id=""><br>
+		<input type="text" name="code" placeholder="Postal / Zip Code" id="">
+		<p>E-mail Address :</p>
+		<input type="email" name="email" placeholder="E-mail" id="">
+		<p>Phone Number :</p>
+		<input type="number" name="phone" placeholder="Phone" id="">
+        <p>Comments :</p>
+		<textarea name="description" id="" cols="25" rows="5"></textarea>
+    <br> </br>
+    <input type="submit">
+    <input type="reset">
+   
+</form> 
+</div>
+</body>
+</html>
+
+
+
